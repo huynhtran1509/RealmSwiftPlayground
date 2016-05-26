18c06f1f2e77eab233294e95f7b7f510899cb085
Andrew Carter <andrew.r.carter@gmail.com>
2016-05-26 16:55:10 -0400

    adds a few more notes about models

--- RealmSwift.playground/Pages/Models.xcplaygroundpage/Contents.swift
+++ RealmSwift.playground/Pages/Models.xcplaygroundpage/Contents.swift
@@ -7,14 +7,25 @@ class Pokemon: Object {
     dynamic var id: Int = 0
     dynamic var type: String = ""
     dynamic var heldItem: String?
+    var prettyDescription: String?
+    
+    override static func indexedProperties() -> [String] {
+        return ["id"]
+    }
+    
+    override static func ignoredProperties() -> [String] {
+        return ["prettyDescription"]
+    }
 }
 
 /*:
  Models inherit from `Object`
- 
+
+ - Supports indexing (faster queries slower inserts)
  - Supports `Bool`, `Int8`, `Int16`, `Int32`, `Int64`, `Double`, `Float`, `String`, `NSDate`, and `NSData`.
  - `String`, `NSDate`, and `NSData` can be `Optional`. `Object` properties *must* be optional.
  - Special `RealmOptional` class can be used for optional numeric types.
+ - Can specify "ignored" properties that Realm won't manage
  - Subclassing is supported with a few catches.
  */
 
